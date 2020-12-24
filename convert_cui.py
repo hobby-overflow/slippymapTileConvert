0fad4837593afdc474e846513952f3c9b9315ff5
hobby-overflow <radioboy.144mhz@gmail.com>
2020-12-24 13:50:57 +0900

    add convert_cui.py

---
+++ convert_cui.py
@@ -0,0 +1,21 @@
+import math
+
+# 参考になったURL
+# https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Python
+
+def deg2num(lat_deg, lon_deg, zoom):
+    lat_rad = math.radians(lat_deg)
+    n = 2.0 ** zoom
+    xtile = int((lon_deg + 180.0) / 360.0 * n)
+    ytile = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
+    return (xtile, ytile)
+
+
+try:
+    lat = int(input("Enter lat: "))
+    lon = int(input("Enter lon: "))
+    zoom = int(input("Enter zoom: "))
+except:
+    print("Error Occured, please number")
+
+print(deg2num(lat, lon, zoom))
